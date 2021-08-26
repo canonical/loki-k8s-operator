d30765286231a4b5b5619b370f7a80d2ae74d070
Jose C. Massón <jose.masson@canonical.com>
2021-08-26 10:51:34 -0300

    Missing copyright notices

--- src/loki_server.py
+++ src/loki_server.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python3
+# Copyright 2021 Canonical Ltd.
+# See LICENSE file for licensing details.
+#
+# Learn more at: https://juju.is/docs/sdk
+
 import logging
 from typing import Optional
 
