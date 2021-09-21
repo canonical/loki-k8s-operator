043c4397a9008959c4d88e8700f405b0178dce46
Jose C. Massón <jose.masson@canonical.com>
2021-09-20 21:45:33 -0300

    Docstrings added

--- lib/charms/loki_k8s/v0/loki.py
+++ lib/charms/loki_k8s/v0/loki.py
@@ -4,6 +4,162 @@
 #
 # Learn more at: https://juju.is/docs/sdk
 
+"""## Overview.
+
+This document explains how to integrate with the Loki charm.
+It also explains how alternative implementations of the Loki charm
+may maintain the same interface and be backward compatible with all
+currently integrated charms. Finally this document is the
+authoritative reference on the structure of relation data that is
+shared between Loki charms and any other charm that intends to
+provide a scrape target for Loki.
+
+## Provider Library Usage
+
+This object may be used by Loki charms to manage relations
+with their clients.
+For this purposes a Loki charm needs to instantiate the
+`LokiProvider` object providing it with two pieces
+of information:
+
+- A reference to the parent (Loki) charm.
+
+- Name of the relation that the Loki charm uses to interact with
+  its clients. This relation must confirm to the
+  `loki_push_api` interface.
+
+For example a Loki charm may instantiate the
+`LokiProvider` in its constructor as follows
+
+    from charms.loki_k8s.v0.loki import LokiProvider
+    from loki_server import LokiServer
+    ...
+
+    class LokiOperatorCharm(CharmBase):
+        ...
+
+        def __init__(self, *args):
+            super().__init__(*args)
+            ...
+            self._provide_loki()
+            ...
+
+        def _provide_loki(self):
+            loki_server = LokiServer()
+
+            if loki_server.is_ready:
+                version = loki_server.version
+                self.loki_provider = LokiProvider(self, "logging")
+                self.unit.status = ActiveStatus()
+                logger.debug("Loki Provider is available. Loki version: %s", version)
+
+
+The `LokiProvider` object has two main responsabilities:
+
+1.- Is in charge of setting the Loki Push API Address into
+relation data so clients can use it to send logs. Every time a unit joins
+a relation: `$ juju add-relation loki-k8s loki-client-k8s` the object sets:
+
+    event.relation.data[self.charm.unit]["data"] = self._loki_push_api
+
+Where `self._loki_push_api` is: `http://<LOKI_UNIT_IP>:<LOKI_PORT>/loki/api/v1/push`
+
+
+2.- Every time a Loki client unit joins a relation set its
+[alerts rules](https://grafana.com/docs/loki/latest/rules/#alerting-rules) to
+relation data (read more in `Alerting Rules`).
+Once these alert rules are sent over relation data, the `LokiProvider` object
+stores these files in the directory `/loki/rules` inside the Loki charm container.
+
+
+## Consumer Library Usage
+
+This Loki charm interacts with its clients using its
+charm library. Charms seeking to send log to the Loki charm,
+must do so using the `LokiConsumer` object from this charm library.
+For the simplest use cases, using the `LokiConsumer` object only requires
+instantiating it, typically in the constructor of your charm (the one which
+sends logs).
+
+    from charms.loki_k8s.v0.loki import LokiConsumer
+
+    class LokiClientCharm(CharmBase):
+
+        def __init__(self, *args):
+            super().__init__(*args)
+            ...
+            self.loki = LokiConsumer(self, "logging")
+
+
+The `LokiConsumer` constructor requires two things:
+
+- A reference to the parent (LokiClientCharm) charm.
+
+- Name of the relation that the Loki charm uses to interact with
+  its clients. This relation must confirm to the
+  `loki_push_api` interface.
+
+
+## Alerting Rules
+
+This charm library also supports gathering alerting rules from all
+related Loki clients charms and enabling corresponding alerts within the
+Loki charm. Alert rules are automatically gathered by `LokiConsumer` object
+from a directory conventionally named `loki_alert_rules`.
+This directory must reside at the top level in the `src` folder of the
+consumer charm. Each file in this directory is assumed to be a single alert rule
+in YAML format. The file name must have the `.rule` extension.
+The format of this alert rule conforms to the
+[Loki docs](https://grafana.com/docs/loki/latest/rules/#alerting-rules).
+An example of the contents of one such file is shown below.
+
+```
+alert: HighPercentageError
+expr: |
+  sum(rate({%%juju_topology%%} |= "error" [5m])) by (job)
+    /
+  sum(rate({%%juju_topology%%}[5m])) by (job)
+    > 0.05
+for: 10m
+labels:
+    severity: page
+annotations:
+    summary: High request latency
+
+```
+
+It is **very important** to note the `%%juju_topology%%` filter in the
+expression for the alert rule shown above. This filter is a stub that
+is automatically replaced by the `LokiConsumer` following Loki Clients's Juju
+topology (application, model and its UUID). Such a topology filter is
+essential to ensure that alert rules submitted by one provider charm
+generates alerts only for that same charm.  The Loki charm may
+be related to multiple Loki client charms. Without this, filter
+rules submitted by one provider charm will also result in
+corresponding alerts for other provider charms. Hence every alert rule
+expression must include such a topology filter stub.
+
+Gathering alert rules and generating rule files within the Loki
+charm is easily done using the `alerts()` method of
+`LokiProvider`. Alerts generated by Loki will
+automatically include Juju topology labels in the alerts. These labels
+indicate the source of the alert. The following lables are
+automatically included with each alert
+
+- `juju_model`
+- `juju_model_uuid`
+- `juju_application`
+
+## Relation Data
+
+The Loki charm uses both application and unit relation data to
+obtain information regarding it Loki Push API and alert rules.
+
+Units of consumer charm sends their alert rules over app relation
+data using the `alert_rules` key.
+
+"""
+
 import json
 import logging
 import os
@@ -42,8 +198,7 @@ class RelationManagerBase(Object):
 
 
 class LokiProvider(RelationManagerBase):
-    """
-    LokiProvider class
+    """A LokiProvider class
     """
 
     def __init__(self, charm, relation_name: str):
@@ -59,25 +214,42 @@ class LokiProvider(RelationManagerBase):
         super().__init__(charm, relation_name)
         self.charm = charm
         self._relation_name = relation_name
-        self.container = self.charm.unit.get_container(self.charm.model.name)
+        self.container = self.charm.unit.get_container("loki")
         events = self.charm.on[relation_name]
         self.framework.observe(events.relation_changed, self._on_logging_relation_changed)
         self.framework.observe(events.relation_departed, self._on_logging_relation_departed)
 
     def _on_logging_relation_changed(self, event):
-        """IMPROVE DOCSTRING"""
+        """Handle changes in related consumers.
+
+        Anytime there are changes in relations between Loki
+        and its consumers charms, Loki set the `loki_push_api`
+        into the relation data.
+        Besides Loki generates alert rules files based what
+        consumer charms forwards,
+
+        Args:
+            event: a `CharmEvent` in response to which the Loki
+                charm must update its relation data.
+        """
 
         if event.relation.data[self.charm.unit].get("data") is None:
             event.relation.data[self.charm.unit]["data"] = self._loki_push_api
             logger.debug("Saving Loki url in relation data %s", self._loki_push_api)
 
         if event.relation.data.get(event.relation.app) is not None:
-            logger.warning("Saving alerts on disk")
+            logger.debug("Saving alerts on disk")
             self._remove_alert_rules_files(self.container)
             self._generate_alert_rules_files(self.container)
 
     def _on_logging_relation_departed(self, event):
-        """IMPROVE DOCSTRING"""
+        """Removes alert rules files when consumer charms
+        left the relation with Loki
+
+        Args:
+            event: a `CharmEvent` in response to which the Loki
+                charm must update its relation data.
+        """
         if event.relation.data.get(event.relation.app) is not None:
             self._remove_alert_rules_files(self.container)
 
@@ -217,14 +389,19 @@ class LokiConsumer(RelationManagerBase):
         self.framework.observe(events.relation_changed, self._on_logging_relaton_changed)
 
     def _on_logging_relaton_changed(self, event):
-        logger.warning("-" * 30)
-        logger.warning(event.unit)
+        """Handle changes in related consumers.
 
+        Anytime there are changes in the relation between Loki
+        and its consumers charms,
+
+        Args:
+            event: a `CharmEvent` in response to which the consumer
+                charm must update its relation data.
+        """
         if event.unit is None:
             # Workaround: Seems this is a Juju bug that sends event.unit == None
             return
 
-        # Get loki_push_api url from relation data
         if data := event.relation.data[event.unit].get("data"):
             self._stored.loki_push_api = json.loads(data)["loki_push_api"]
 
@@ -232,6 +409,12 @@ class LokiConsumer(RelationManagerBase):
         self._set_alert_rules(event)
 
     def _set_alert_rules(self, event):
+        """Set alert rules into relation data.
+
+        Args:
+            event: a `CharmEvent` in response to which the consumer
+                charm must update its relation data.
+        """
         event.relation.data[self._charm.unit]["loki_host"] = str(
             self._charm.model.get_binding(event.relation).network.bind_address
         )
