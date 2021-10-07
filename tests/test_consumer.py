c0eb3f4ab209c8cf14d99688a088769a6d6051c5
Jose C. Massón <jose.masson@canonical.com>
2021-10-07 10:41:32 -0300

    Improve test

--- tests/test_consumer.py
+++ tests/test_consumer.py
@@ -74,8 +74,14 @@ class TestLokiConsumer(unittest.TestCase):
         self.assertEqual(self.harness.update_relation_data(rel_id, "promtail/0", {}), None)
 
     def test__on_logging_relation_changed_no_unit(self):
+        self.harness.set_leader(True)
         rel_id = self.harness.add_relation("logging", "promtail")
-        self.assertEqual(self.harness.update_relation_data(rel_id, "promtail", {}), None)
+        self.harness.add_relation_unit(rel_id, "promtail/0")
+        self.assertEqual(self.harness.update_relation_data(
+            rel_id,
+            "promtail",
+            {"data": '{"loki_push_api": "http://10.1.2.3:3100/loki/api/v1/push"}'},
+        ), None)
 
     @patch("charms.loki_k8s.v0.loki_push_api.LokiConsumer._labeled_alert_groups", new_callable=PropertyMock)
     def test__on_logging_relation_changed(self, mock_alert_rules):
