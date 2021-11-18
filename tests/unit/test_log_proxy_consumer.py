64230e67d9225779ee36f49cb23de25a0e8f641f
Jose C. Massón <jose.masson@canonical.com>
2021-11-18 02:23:42 -0300

    Two more tests

--- tests/unit/test_log_proxy_consumer.py
+++ tests/unit/test_log_proxy_consumer.py
@@ -1,28 +1,15 @@
 # Copyright 2020 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-import os
 import textwrap
 import unittest
 from unittest.mock import patch
 
 from charms.loki_k8s.v0.log_proxy import LogProxyConsumer
+from deepdiff import DeepDiff  # type: ignore
 from ops.charm import CharmBase
 from ops.framework import StoredState
 from ops.testing import Harness
-
-
-def pull_empty_fake_file(self, _):
-    return FakeFile("")
-
-
-class FakeFile:
-    def __init__(self, content="PEPE"):
-        self.content = content
-
-    def read(self, *args, **kwargs):
-        return self.content
-
 
 METADATA = {
     "model": "consumer-model",
@@ -37,16 +24,47 @@ LOG_FILES = [
     "/var/log/test.log",
 ]
 
-CONFIG: dict = {"clients": []}
+INIT_CONFIG: dict = {"clients": []}
 
+CONFIG = {
+    "clients": [{"url": "http://10.20.30.1:3500/loki/api/v1/push"}],
+    "server": {"http_listen_port": 9080, "grpc_listen_port": 9095},
+    "positions": {"filename": "/opt/promtail/positions.yaml"},
+    "scrape_configs": [
+        {
+            "job_name": "system",
+            "static_configs": [
+                {
+                    "targets": ["localhost"],
+                    "labels": {
+                        "job": "juju_MODEL_123456_loki-k8s",
+                        "__path__": "/var/log/apache2/access.log",
+                    },
+                },
+                {
+                    "targets": ["localhost"],
+                    "labels": {
+                        "job": "juju_MODEL_123456_loki-k8s",
+                        "__path__": "/var/log/alternatives.log",
+                    },
+                },
+                {
+                    "targets": ["localhost"],
+                    "labels": {
+                        "job": "juju_MODEL_123456_loki-k8s",
+                        "__path__": "/var/log/test.log",
+                    },
+                },
+            ],
+        }
+    ],
+}
 
 WORKLOAD_BINARY_DIR = "/opt/promtail"
 WORKLOAD_POSITIONS_PATH = "{}/positions.yaml".format(WORKLOAD_BINARY_DIR)
 
 HTTP_LISTEN_PORT = 9080
 GRPC_LISTEN_PORT = 9095
-
-os.environ["JUJU_MODEL_NAME"] = "PEPEEEE"
 
 
 class ConsumerCharm(CharmBase):
@@ -118,3 +136,30 @@ class TestLogProxyConsumer(unittest.TestCase):
             ],
         }
         self.assertEqual(self.harness.charm._log_proxy._initial_config, expected)
+
+    def test__add_client(self):
+        agent_url1 = "http://10.20.30.1:3500/loki/api/v1/push"
+        agent_url2 = "http://10.20.30.2:3500/loki/api/v1/push"
+
+        expected_config = CONFIG.copy()
+        expected_config["clients"] = [{"url": agent_url1}, {"url": agent_url2}]
+        self.harness.charm._log_proxy._add_client(CONFIG, agent_url1)
+        conf = self.harness.charm._log_proxy._add_client(CONFIG, agent_url2)
+        self.assertEqual(DeepDiff(conf, expected_config, ignore_order=True), {})
+
+        CONFIG.pop("clients")
+        expected_config2 = CONFIG.copy()
+        expected_config2["clients"] = [{"url": agent_url1}, {"url": agent_url2}]
+        self.harness.charm._log_proxy._add_client(CONFIG, agent_url1)
+        conf1 = self.harness.charm._log_proxy._add_client(CONFIG, agent_url2)
+        self.assertEqual(DeepDiff(conf1, expected_config2, ignore_order=True), {})
+
+    def test__remove_client(self):
+        agent_url1 = "http://10.20.30.1:3500/loki/api/v1/push"
+        agent_url2 = "http://10.20.30.2:3500/loki/api/v1/push"
+        CONFIG["clients"] = [{"url": agent_url1}, {"url": agent_url2}]
+        expected_config: dict = CONFIG.copy()
+        expected_config["clients"].pop(0)
+
+        conf = self.harness.charm._log_proxy._remove_client(CONFIG, agent_url1)
+        self.assertEqual(DeepDiff(conf, expected_config, ignore_order=True), {})
