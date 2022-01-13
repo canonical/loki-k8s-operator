3d5c0ef86276bd96dfd36fe4046cf6bc52e34ab9
Jose C. Massón <jose.masson@canonical.com>
2022-01-13 09:51:18 -0300

    Fix docstring

--- lib/charms/loki_k8s/v0/loki_push_api.py
+++ lib/charms/loki_k8s/v0/loki_push_api.py
@@ -209,7 +209,7 @@ Adopting this object in a charmed operator consist of two steps:
            ...
            try:
                self._log_proxy = LogProxyConsumer(
-                   charm=self, log_files=LOG_FILES, container_name=PEER, syslog=True
+                   charm=self, log_files=LOG_FILES, container_name=PEER, enable_syslog=True
                )
            except PromtailDigestError as e:
                msg = str(e)
