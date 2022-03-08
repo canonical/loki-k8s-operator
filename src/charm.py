4aacddc15ebc0ba2ee3616706139a0010a06bfab
Jose C. Massón <jose.masson@canonical.com>
2022-03-08 11:54:31 -0300

    Delete commented line

--- src/charm.py
+++ src/charm.py
@@ -168,7 +168,6 @@ class LokiOperatorCharm(CharmBase):
         """Gets LokiPushApiProvider instance into `self.loki_provider`."""
         try:
             version = self._loki_server.version
-            # self.loki_provider = self.loki_provider or LokiPushApiProvider(self)
             logger.debug("Loki Provider is available. Loki version: %s", version)
         except LokiServerNotReadyError as e:
             self.unit.status = WaitingStatus(str(e))
