ce76ffd7a528c1ad77b11ca91c31cc40538a10a2
Jose C. Massón <jose.masson@canonical.com>
2022-03-14 11:15:09 -0300

    Add itest that checks loki is blocked when error alert rules are sent

--- tests/integration/test_alert_rules.py
+++ tests/integration/test_alert_rules.py
@@ -18,6 +18,7 @@ app_name = METADATA["name"]
 resources = {"loki-image": METADATA["resources"]["loki-image"]["upstream-source"]}
 rules_app = "cos-config"
 rules_app2 = "cos-config2"
+rules_app3 = "cos-config3"
 
 
 @pytest.mark.abort_on_fail
@@ -115,3 +116,31 @@ async def test_remove_app_one_alert_rules_is_reteined(ops_test):
 
     rules_after_delete_relation2 = await loki_rules(ops_test, app_name)
     assert len(rules_after_delete_relation2) == 1
+
+
+@pytest.mark.abort_on_fail
+async def test_wrong_alert_rule(ops_test):
+    await asyncio.gather(
+        ops_test.model.deploy(
+            "ch:cos-configuration-k8s",
+            application_name=rules_app3,
+            channel="edge",
+            config={
+                "git_repo": "https://github.com/canonical/loki-k8s-operator",
+                "git_branch": "error_alerts",
+                "loki_alert_rules_path": "tests/sample_rule_files/free-standing/error",
+            },
+        ),
+    )
+
+    # force an update, just in case the files showed up on disk after the last hook fired
+    action = await ops_test.model.applications[rules_app3].units[0].run_action("sync-now")
+    await action.wait()
+
+    # form a relation between loki and the app that provides rules
+    await ops_test.model.add_relation(app_name, rules_app3)
+
+    async with IPAddressWorkaround(ops_test):
+        await ops_test.model.wait_for_idle(
+            apps=[app_name], status="blocked", timeout=1000
+        )
