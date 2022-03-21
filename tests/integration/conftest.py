d185b1cce4249c38ff094ee48604a9bb79182d81
Balbir Thomas <balbir.thomas@canonical.com>
2022-03-21 14:47:33 +0000

    A workload free Loki Tester Charm
    This commit introduces a Loki tester charm for the purpose
    of integration testing the Loki charm. The loki tester
    charm forwards its own Python debug logs to Loki, when
    a Juju relation is created between it and the Loki charm.
    Normally the tester charm only forwards Python debug logs
    to Loki. The tester charm also provides and Juju action
    "log-error" which logs and error message to loki. The
    tester charm contains an alert rule which is also forwarded
    to Loki. This alert rule triggers an alert any time
    an error log is received by Loki.

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -2,6 +2,7 @@
 # See LICENSE file for licensing details.
 
 import pytest
+import shutil
 from pytest_operator.plugin import OpsTest
 
 
@@ -9,4 +10,19 @@ from pytest_operator.plugin import OpsTest
 async def loki_charm(ops_test: OpsTest):
     """Loki charm used for integration testing."""
     charm = await ops_test.build_charm(".")
+    return charm
+
+@pytest.fixture(scope="module", autouse=True)
+def copy_loki_library_into_tester_charm(ops_test):
+    """Ensure that the tester charm uses the current Prometheus library."""
+    library_path = "lib/charms/loki_k8s/v0/loki_push_api.py"
+    install_path = "tests/integration/loki-tester/" + library_path
+    shutil.copyfile(library_path, install_path)
+
+
+@pytest.fixture(scope="module")
+async def loki_tester_charm(ops_test):
+    """A charm for integration test of the Loki charm."""
+    charm_path = "tests/integration/loki-tester"
+    charm = await ops_test.build_charm(charm_path)
     return charm
---
+++ tests/integration/loki-tester/src/charm.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+# Copyright 2022 Balbir Thomas
+# See LICENSE file for licensing details.
+#
+# Learn more at: https://juju.is/docs/sdk
+
+"""A Integration tester charm for Loki Operator."""
+
+import logging
+import logging_loki
+
+from ops.charm import CharmBase
+from ops.main import main
+from ops.model import ActiveStatus
+
+from charms.loki_k8s.v0.loki_push_api import LokiPushApiConsumer, ProviderTopology
+
+
+class LokiTesterCharm(CharmBase):
+    """A Loki Operator Client Charm."""
+
+    def __init__(self, *args):
+        super().__init__(*args)
+
+        self._loki_consumer = LokiPushApiConsumer(self)
+
+        self.framework.observe(self.on.config_changed, self._on_config_changed)
+        self.framework.observe(self.on.update_status, self._on_update_status)
+        self.framework.observe(self.on.log_error_action, self._on_log_error_action)
+        self.framework.observe(
+            self._loki_consumer.on.loki_push_api_endpoint_joined,
+            self._on_loki_push_api_endpoint_joined,
+        )
+        self.framework.observe(
+            self._loki_consumer.on.loki_push_api_endpoint_departed,
+            self._on_loki_push_api_endpoint_departed,
+        )
+
+        self.topology = ProviderTopology.from_charm(self)
+
+        self.logger = None
+        self.log_handler = None
+        self.log_endpoints = []
+        self.set_logger()
+
+        self.log("debug", "Constructed loki tester")
+
+    def _on_config_changed(self, _):
+        """Handle changed configuration."""
+        self.log("debug", "Handling configuration change")
+        self.unit.status = ActiveStatus()
+
+    def _on_update_status(self, _):
+        self.log("debug", "Updating logger status")
+        self.unit.status = ActiveStatus()
+
+    def _on_loki_push_api_endpoint_joined(self, _):
+        self.log("debug", "Loki push API endpoint joined")
+        self.set_logger()
+
+    def _on_loki_push_api_endpoint_departed(self, _):
+        self.log("debug", "Loki push API endpoint departed")
+        # TODO (multi-logger): remove only the logger whoe's endpoint departed
+        self.logger = None
+
+    def _on_log_error_action(self, event):
+        message = event.params["message"]
+        logged = self.log("error", message)
+        if logged:
+            event.set_results({"message": "Error message successfully logged"})
+        else:
+            event.fail("Failed to log error message")
+
+    def set_logger(self):
+        tags = self.topology.as_promql_label_dict()
+        log_endpoints = self._loki_consumer.loki_endpoints
+
+        if log_endpoints:
+            logging_loki.emitter.LokiEmitter.level_tag = "level"
+            # TODO (multi-logger): create logggers for each endpoint
+            self.log_handler = logging_loki.LokiHandler(
+                url=log_endpoints[0]["url"],
+                version="1",
+                tags=dict(tags)
+            )
+            # TODO (multi-logger): each logger will need a different name
+            self.logger = logging.getLogger("Loki-Tester")
+            self.logger.addHandler(self.log_handler)
+
+            self.log("debug", "Successfuly set Loki Logger")
+
+    def log(self, level, msg):
+        try:
+            getattr(self.logger, level)(msg)
+            return True
+        except AttributeError:
+            return False
+
+
+if __name__ == "__main__":
+    main(LokiTesterCharm)
