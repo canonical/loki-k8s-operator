a168c75217812e451283dc48d272e5ac5f33681f
Pietro Pasotti <starfire.daemon@gmail.com>
2022-03-29 15:17:06 +0200

    restructured itest, simplified flow

--- tests/integration/test_charm_with_loki.py
+++ tests/integration/test_charm_with_loki.py
@@ -21,7 +21,9 @@ from tenacity import retry, stop_after_attempt, wait_exponential
 
 # copied over from log.py to avoid circular imports
 TEST_JOB_NAME = "test-job"
-SYSLOG_LOG_MSG = "LOG SYSLOG"
+# for some reason the syslog log message gets decoded with
+# this \ufeff thing prepended
+SYSLOG_LOG_MSG = "\ufeffLOG SYSLOG"
 LOKI_LOG_MSG = "LOG LOKI"
 FILE_LOG_MSG = "LOG FILE"
 
@@ -125,77 +127,95 @@ async def test_loki_scraping_with_promtail(
     # at this point the workload should run and fire the logs to the configured targets
     await ops_test.juju("config", loki_tester_app_name, f"log-to={','.join(modes)}")
     await ops_test.model.wait_for_idle(apps=[loki_tester_app_name], status="active")
-    await asyncio.gather(
-        *(assert_logs_in_loki(mode, loki_app_name, ops_test, loki_address) for mode in modes)
+
+    # by now all logs should be in loki. if this races, we might want to
+    # introduce a little sleep here, or await loki to show something for a
+    # little while.
+
+    # get all existing job labels
+    jobs = (
+        requests.get(f"http://{loki_address}:3100/loki/api/v1/label/job/values").json().get("data")
     )
+    assert jobs, "no jobs present: nothing was logged yet."
+
+    checks = []
+
+    if "loki" in modes:
+        assert TEST_JOB_NAME in jobs
+        checks.append(assert_job_logged(TEST_JOB_NAME, LOKI_LOG_MSG, loki_address))
+        stream = {"job": TEST_JOB_NAME}
+        checks.append(assert_stream_equals(TEST_JOB_NAME, loki_address, stream))
+
+    if "file" in modes:
+        job = next(filter(lambda x: x.endswith("loki-tester"), jobs))
+        checks.append(assert_job_logged(job, FILE_LOG_MSG, loki_address))
+
+        extra_stream = {"filename": "/loki_tester_msgs.txt"}
+        checks.append(
+            assert_stream_matches_template(
+                job, loki_address, stream_template, extra_stream=extra_stream
+            )
+        )
+    if "syslog" in modes:
+        job = next(filter(lambda x: x.endswith("loki-tester_syslog"), jobs))
+        checks.append(assert_job_logged(job, SYSLOG_LOG_MSG, loki_address))
+        checks.append(assert_stream_matches_template(job, loki_address, stream_template))
+
+    await asyncio.gather(*checks)
 
 
+juju_model_uuid_key = "juju_model_uuid"
+promtail_job_template = "juju_{model_name}_{uuid}_loki-tester_loki-tester_0_loki-tester"
 stream_template = {
-    "job": "juju_{model_name}_{uuid}_loki-tester_loki-tester_0_loki-tester",
     "juju_application": "loki-tester",
     "juju_charm": "loki-tester",
     "juju_model": "{model_name}",
-    "juju_model_uuid": "{uuid}",
+    juju_model_uuid_key: "{uuid}",
     "juju_unit": "loki-tester/0",
 }
 
 
-async def assert_logs_in_loki(mode: str, loki_app_name: str, ops_test: OpsTest, loki_address: str):
-    url = f"http://{loki_address}:3100"
+def query_range(job_name: str, loki_address: str):
+    params = {"query": '{job="%s"}' % job_name}
+    # query_range goes from now to up to 1h ago, more
+    # certain to capture something
+    query_url = f"http://{loki_address}:3100/loki/api/v1/query_range"
+    jsn = requests.get(query_url, params=params).json()
+    return jsn["data"]["result"]
 
-    async def query_job(job_name, attempt=0):
-        params = {"query": '{job="%s"}' % job_name}
-        # query_range goes from now to up to 1h ago, more
-        # certain to capture something
-        query_url = f"{url}/loki/api/v1/query_range"
-        jsn = requests.get(query_url, params=params).json()
-        results = jsn["data"]["result"]
-        if not results:
-            raise ValueError(f"{query_url} returned an empty result set.")
-        result = results[0]
-        return result
 
-    if mode == "loki":
-        res = await query_job(TEST_JOB_NAME)
-        assert res["stream"] == {"job": TEST_JOB_NAME}
-        assert res["values"][0][1] == LOKI_LOG_MSG
-        return
+async def assert_job_logged(job_name: str, msg: str, loki_address: str):
+    results = query_range(job_name, loki_address)
+    assert results, f"{loki_address} returned an empty result set."
+    result = results[0]
+    assert result["values"][0][1] == msg
 
-    if mode == "file":
-        suffix = "_loki-tester"
-        msg = FILE_LOG_MSG
-        extra_stream = {"filename": "/loki_tester_msgs.txt"}
-        job_suffix = ""
-    else:  # syslog mode
-        suffix = "_loki-tester_syslog"
-        # need to prefix BOM \ufeff because syslog msgs are encoded in some
-        # special way. 'utf-8-sig' perhaps?
-        msg = "\ufeff" + SYSLOG_LOG_MSG
-        extra_stream = {}
-        job_suffix = "_syslog"
 
-    # get the job name for the log stream
-    labels = requests.get(f"{url}/loki/api/v1/label/job/values").json().get("data")
-    if not labels:
-        raise RuntimeError("no jobs present: nothing was logged yet.")
-    try:
-        job_label = next(filter(lambda x: x.endswith(suffix), labels))
-    except StopIteration:
-        raise RuntimeError(f"expected label with suffix {suffix} not " f"found in" f"{labels}.")
-
-    print("job label:", job_label)
-
-    res = await query_job(job_label)
-    juju_model_uuid_key = "juju_model_uuid"
-    juju_model_uuid = res["stream"][juju_model_uuid_key]
-    expected_stream = stream_template.copy()
-    expected_stream.update(extra_stream)
-    expected_stream["juju_model"] = juju_model_name = res["stream"]["juju_model"]
+def populate_template(template, result):
+    juju_model_uuid = result["stream"][juju_model_uuid_key]
+    expected_stream = template.copy()
+    expected_stream["juju_model"] = juju_model_name = result["stream"]["juju_model"]
     expected_stream[juju_model_uuid_key] = juju_model_uuid
-    expected_stream["job"] = (
-        expected_stream["job"].format(uuid=juju_model_uuid, model_name=juju_model_name)
-        + job_suffix
+    expected_stream["job"] = promtail_job_template.format(
+        uuid=juju_model_uuid, model_name=juju_model_name
     )
+    return expected_stream
 
-    assert res["stream"] == expected_stream
-    assert res["values"][0][1] == msg
+
+async def assert_stream_matches_template(job_name, loki_address, template, extra_stream=None):
+    results = query_range(job_name, loki_address)
+    # we fill in the stream template with model uuid and model name, which vary.
+    # any result will do for this, as they should be all the same.
+    expected_stream = populate_template(template, results[0])
+
+    if extra_stream:
+        expected_stream.update(extra_stream)
+
+    for result in results:
+        assert result["stream"] == expected_stream
+
+
+async def assert_stream_equals(job_name, loki_address, stream):
+    results = query_range(job_name, loki_address)
+    for result in results:
+        assert result["stream"] == stream
