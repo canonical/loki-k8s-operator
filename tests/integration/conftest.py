c7e25d66ad8d5f158378910e3e54f33a71f1462b
Ryan Barry <ryan.barry@canonical.com>
2022-04-05 14:11:13 -0400

    Alertmanager Integration (#129)
    * A workload free Loki Tester Charm
    
    This commit introduces a Loki tester charm for the purpose
    of integration testing the Loki charm. The loki tester
    charm forwards its own Python debug logs to Loki, when
    a Juju relation is created between it and the Loki charm.
    Normally the tester charm only forwards Python debug logs
    to Loki. The tester charm also provides and Juju action
    "log-error" which logs and error message to loki. The
    tester charm contains an alert rule which is also forwarded
    to Loki. This alert rule triggers an alert any time
    an error log is received by Loki.
    
    * An alerting integration test
    
    This commit adds an integration test to check that
    LokiPushApiConsumer can forward logs and alert rules
    to LokiPushApiProvider. The test further checks
    conditions satisfying the alert rules do indeed raise
    alerts.
    
    * Clean up all the linter warnings and add integration tests for alertmanager
    
    * Fix attribute error due to typo in method name
    
    * Integration test updates
    
    Add a helper method to also check the Prometheus-compatible
    alerts endpoint from Loki itself, so we can check there as a
    bisection point between Alertmanager.
    
    Add a test which exercises this endpoint.
    
    Typos and cleanup.
    
    Add a (very temporary) FIXME so the "other" test which uses
    Alertmananager can be moved to the bundle when this merges
    
    * Make the linters happy
    
    Co-authored-by: Balbir Thomas <balbir.thomas@canonical.com>

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,5 +1,6 @@
 # Copyright 2021 Canonical Ltd.
 # See LICENSE file for licensing details.
+import shutil
 
 import pytest
 from pytest_operator.plugin import OpsTest
@@ -9,4 +10,20 @@ from pytest_operator.plugin import OpsTest
 async def loki_charm(ops_test: OpsTest):
     """Loki charm used for integration testing."""
     charm = await ops_test.build_charm(".")
+    return charm
+
+
+@pytest.fixture(scope="module", autouse=True)
+def copy_loki_library_into_tester_charm(ops_test):
+    """Ensure that the tester charm uses the current Prometheus library."""
+    library_path = "lib/charms/loki_k8s/v0/loki_push_api.py"
+    install_path = "tests/integration/loki-tester/" + library_path
+    shutil.copyfile(library_path, install_path)
+
+
+@pytest.fixture(scope="module")
+async def loki_tester_charm(ops_test):
+    """A charm for integration test of the Loki charm."""
+    charm_path = "tests/integration/loki-tester"
+    charm = await ops_test.build_charm(charm_path)
     return charm
--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -1,9 +1,11 @@
 # Copyright 2021 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+import asyncio
 import json
 import logging
 import urllib.request
+from typing import List
 
 import yaml
 from pytest_operator.plugin import OpsTest
@@ -41,6 +43,114 @@ async def loki_rules(ops_test, app_name) -> dict:
         return {}
     except urllib.error.HTTPError:
         return {}
+
+
+async def loki_alerts(ops_test: str, app_name: str, unit_num: int = 0, retries: int = 3) -> dict:
+    r"""Get a list of alerts from a Prometheus-compatible endpoint.
+
+    Results look like:
+        {
+          "data": {
+              "groups": [
+                  {
+                      "rules": [
+                          {
+                              "alerts": [
+                                  {
+                                      "activeAt": "2018-07-04T20:27:12.60602144+02:00",
+                                      "annotations": {
+                                          "summary": "High request latency"
+                                      },
+                                      "labels": {
+                                          "alertname": "HighRequestLatency",
+                                          "severity": "page"
+                                      },
+                                      "state": "firing",
+                                      "value": "1e+00"
+                                  }
+                              ],
+                              "annotations": {
+                                  "summary": "High request latency"
+                              },
+                              "duration": 600,
+                              "health": "ok",
+                              "labels": {
+                                  "severity": "page"
+                              },
+                              "name": "HighRequestLatency",
+                              "query": "job:request_latency_seconds:mean5m{job=\"myjob\"} > 0.5",
+                              "type": "alerting"
+                          },
+                          {
+                              "health": "ok",
+                              "name": "job:http_inprogress_requests:sum",
+                              "query": "sum by (job) (http_inprogress_requests)",
+                              "type": "recording"
+                          }
+                      ],
+                      "file": "/rules.yaml",
+                      "interval": 60,
+                      "limit": 0,
+                      "name": "example"
+                  }
+              ]
+          },
+          "status": "success"
+        }
+    """
+    address = await get_unit_address(ops_test, app_name, unit_num)
+    url = f"http://{address}:3100/prometheus/api/v1/alerts"
+
+    # Retry since the endpoint may not _immediately_ return valid data
+    while not (
+        alerts := json.loads(urllib.request.urlopen(url, data=None, timeout=2).read())["data"][
+            "alerts"
+        ]
+    ):
+        retries -= 1
+        if retries > 0:
+            await asyncio.sleep(2)
+        else:
+            break
+
+    return alerts
+
+
+async def get_alertmanager_alerts(ops_test: OpsTest, unit_name, unit_num, retries=3) -> List[dict]:
+    """Get a list of alerts.
+
+    Response looks like this:
+    {
+        'annotations': {'description': 'test-charm-...', 'summary': 'Instance test-charm-...'},
+        'endsAt': '2021-09-03T21:03:59.658Z',
+        'fingerprint': '4a0016cc12a07903',
+        'receivers': [{'name': 'pagerduty'}],
+        'startsAt': '2021-09-03T19:37:59.658Z',
+        'status': {'inhibitedBy': [], 'silencedBy': [], 'state': 'active'},
+        'updatedAt': '2021-09-03T20:59:59.660Z',
+        'generatorURL': 'http://prometheus-0:9090/...',
+        'labels': {
+            'alertname': 'AlwaysFiring',
+            'instance': 'test-charm-...',
+            'job': 'juju_test-charm-...',
+            'juju_application': 'tester', 'juju_model': 'test-charm-...',
+            'juju_model_uuid': '...',
+            'juju_unit': 'tester-0',
+            'severity': 'Low',
+            'status': 'testing'
+        }
+    }
+    """
+    address = await get_unit_address(ops_test, unit_name, unit_num)
+    url = f"http://{address}:9093/api/v2/alerts"
+    while not (alerts := json.loads(urllib.request.urlopen(url, data=None, timeout=2).read())):
+        retries -= 1
+        if retries > 0:
+            await asyncio.sleep(2)
+        else:
+            break
+
+    return alerts
 
 
 class IPAddressWorkaround:
