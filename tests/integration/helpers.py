a8484d39ec524a0b352c86c3f3a2a1e108cabd2b
Ryan Barry <ryan.barry@canonical.com>
2022-04-28 21:42:43 +0000

    Kill IPAddressWorkaround

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -153,33 +153,6 @@ async def get_alertmanager_alerts(ops_test: OpsTest, unit_name, unit_num, retrie
     return alerts
 
 
-class IPAddressWorkaround:
-    """Context manager for deploying a charm that needs to have its IP address.
-
-    Due to a juju bug, occasionally some charms finish a startup sequence without
-    having an ip address returned by `bind_address`.
-    https://bugs.launchpad.net/juju/+bug/1929364
-
-    On entry, the context manager changes the update status interval to the minimum 10s, so that
-    the update_status hook is trigger shortly.
-    On exit, the context manager restores the interval to its previous value.
-    """
-
-    def __init__(self, ops_test: OpsTest):
-        self.ops_test = ops_test
-
-    async def __aenter__(self):
-        """On entry, the update status interval is set to the minimum 10s."""
-        config = await self.ops_test.model.get_config()
-        self.revert_to = config["update-status-hook-interval"]
-        await self.ops_test.model.set_config({"update-status-hook-interval": "10s"})
-        return self
-
-    async def __aexit__(self, exc_type, exc_value, exc_traceback):
-        """On exit, the update status interval is reverted to its original value."""
-        await self.ops_test.model.set_config({"update-status-hook-interval": self.revert_to})
-
-
 class ModelConfigChange:
     """Context manager for temporarily changing a model config option."""
 
