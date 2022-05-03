99cfad7043c2e10953c92cd1df89e472727550a3
Ryan Barry <ryan.barry@canonical.com>
2022-05-03 09:31:04 +0000

    Re-add scale back down to zero

--- tests/integration/test_alert_rules_fire.py
+++ tests/integration/test_alert_rules_fire.py
@@ -8,7 +8,7 @@ from pathlib import Path
 
 import pytest
 import yaml
-from helpers import get_alertmanager_alerts, is_loki_up, loki_alerts
+from helpers import get_alertmanager_alerts, is_loki_up, juju_show_unit, loki_alerts
 
 logger = logging.getLogger(__name__)
 
@@ -93,6 +93,24 @@ async def test_loki_scales_up(ops_test):
             for key in ["juju_application", "juju_model", "juju_model_uuid"]
             for alert in unit_alerts
         )
+
+
+@pytest.mark.abort_on_fail
+async def test_scale_down_to_zero_units(ops_test):
+    loki_app_name = "loki"
+    await ops_test.model.applications[loki_app_name].scale(scale=0)
+    await ops_test.model.wait_for_idle(
+        apps=[loki_app_name], status="active", timeout=600, wait_for_exact_units=0
+    )
+
+    loki_data_on_requirer_side = await juju_show_unit(
+        ops_test,
+        "loki-tester/0",
+        endpoint="logging",
+        app_data_only=True,
+    )
+
+    assert "related-units" not in loki_data_on_requirer_side["relation-info"][0]
 
     # FIXME: move the following test to the bundle and end the file here once merged and we can
     # Clean up the model so the next test can run
