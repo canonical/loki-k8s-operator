13a1a5f46a3717ba6c451b4e743ed1899ddabf61
Ryan Barry <ryan.barry@canonical.com>
2022-05-05 02:17:49 +0000

    Remove commented test code

--- tests/integration/test_log_proxy.py
+++ tests/integration/test_log_proxy.py
@@ -19,7 +19,6 @@ tester_resources = {
 
 loki_app_name = "loki"
 tester_app_name = "log-proxy-tester"
-rules_app_name = "cos-config"
 
 
 @pytest.mark.abort_on_fail
@@ -58,81 +57,3 @@ async def test_alert_rules_do_fire(ops_test, loki_charm, log_proxy_tester_charm)
         for key in ["juju_application", "juju_model", "juju_model_uuid"]
         for alert in alerts
     )
-
-
-# @pytest.mark.abort_on_fail
-# async def test_alert_rules_do_forward_to_alertmanager(ops_test, loki_charm, loki_tester_charm):
-#     """Test basic functionality of Loki push API relation interface."""
-#     loki_app_name = "loki"
-#     tester_app_name = "log-proxy-tester"
-#     alertmanager_app_name = "alertmanager"
-#     app_names = [loki_app_name, tester_app_name, alertmanager_app_name]
-#
-#     await ops_test.model.set_config({"logging-config": "<root>=WARNING; unit=DEBUG"})
-#     await asyncio.gather(
-#         ops_test.model.deploy(
-#             loki_charm,
-#             resources=resources,
-#             application_name=loki_app_name,
-#         ),
-#         ops_test.model.deploy(
-#             "ch:flog-k8s",
-#             application_name=tester_app_name,
-#             channel="edge",
-#         ),
-#         ops_test.model.deploy(
-#             "ch:cos-configuration-k8s",
-#             application_name=tester_app_name,
-#             channel="edge",
-#             config={
-#                 "git_repo": "https://github.com/canonical/loki-k8s-operator",
-#                 "git_branch": "test_rules/log_proxy",
-#                 "loki_alert_rules_path": "tests/sample_rule_files/free-standing",
-#             },
-#         ),
-#     )
-#
-#     await asyncio.gather(
-#         ops_test.model.deploy(
-#             loki_charm,
-#             resources=resources,
-#             application_name=loki_app_name,
-#         ),
-#         ops_test.model.deploy(
-#             loki_tester_charm,
-#             application_name=tester_app_name,
-#         ),
-#         ops_test.model.deploy(
-#             "ch:alertmanager-k8s",
-#             application_name=alertmanager_app_name,
-#             channel="edge",
-#         ),
-#     )
-#
-#     await ops_test.model.block_until(
-#         lambda: (
-#             len(ops_test.model.applications[loki_app_name].units) > 0
-#             and len(ops_test.model.applications[tester_app_name].units) > 0
-#             and len(ops_test.model.applications[alertmanager_app_name].units) > 0
-#         )
-#     )
-#     await ops_test.model.wait_for_idle(apps=app_names, status="active")
-#
-#     await asyncio.gather(
-#         ops_test.model.add_relation(loki_app_name, tester_app_name),
-#         ops_test.model.add_relation(loki_app_name, alertmanager_app_name),
-#     )
-#     await ops_test.model.wait_for_idle(
-#         apps=[loki_app_name, tester_app_name, alertmanager_app_name], status="active"
-#     )
-#
-#     # Trigger a log message to fire an alert on
-#     await ops_test.model.applications[tester_app_name].units[0].run_action(
-#         "log-error", message="Error logged!"
-#     )
-#     alerts = await get_alertmanager_alerts(ops_test, "alertmanager", 0, retries=100)
-#     assert all(
-#         key in alert["labels"].keys()
-#         for key in ["juju_application", "juju_model", "juju_model_uuid"]
-#         for alert in alerts
-#     )
