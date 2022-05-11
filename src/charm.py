e202b3dfdab654d5da930c0a77b3c224736e3789
Jose C. Massón <jose.masson@canonical.com>
2022-05-11 15:40:15 -0300

    Update charm.py

--- src/charm.py
+++ src/charm.py
@@ -14,7 +14,10 @@ develop a new k8s charm using the Operator Framework:
 
 import logging
 import os
+import socket
 import textwrap
+from urllib import request
+from urllib.error import HTTPError, URLError
 from urllib.parse import urlparse
 
 import yaml
@@ -30,7 +33,7 @@ from ops.charm import CharmBase
 from ops.framework import StoredState
 from ops.main import main
 from ops.model import ActiveStatus, BlockedStatus, WaitingStatus
-from ops.pebble import PathError, ProtocolError
+from ops.pebble import Layer, PathError, ProtocolError
 
 from loki_server import LokiServer, LokiServerError, LokiServerNotReadyError
 
@@ -38,8 +41,6 @@ from loki_server import LokiServer, LokiServerError, LokiServerNotReadyError
 LOKI_CONFIG = "/etc/loki/local-config.yaml"
 LOKI_DIR = "/loki"
 RULES_DIR = os.path.join(LOKI_DIR, "rules")
-PEER = "loki"
-LOKI_PEERS = "loki-peers"
 
 logger = logging.getLogger(__name__)
 
@@ -49,13 +50,21 @@ class LokiOperatorCharm(CharmBase):
 
     _stored = StoredState()
     _port = 3100
-    _name = PEER
+    _name = "loki"
 
     def __init__(self, *args):
         super().__init__(*args)
         self._container = self.unit.get_container(self._name)
         self._stored.set_default(k8s_service_patched=False, config="")
+
+        # If Loki is run in single-tenant mode, all the chunks are put in a folder named "fake"
+        # https://grafana.com/docs/loki/latest/operations/storage/filesystem/
+        # https://grafana.com/docs/loki/latest/rules/#ruler-storage
+        tenant_id = "fake"
+        self._rules_dir = os.path.join(RULES_DIR, tenant_id)
+
         self.service_patch = KubernetesServicePatch(self, [(self.app.name, self._port)])
+
         self.alertmanager_consumer = AlertmanagerConsumer(self, relation_name="alertmanager")
         self.ingress_per_unit = IngressPerUnitRequirer(
             self, relation_name="ingress", port=self._port
@@ -64,17 +73,11 @@ class LokiOperatorCharm(CharmBase):
         self.grafana_source_provider = GrafanaSourceProvider(
             charm=self,
             refresh_event=self.on.loki_pebble_ready,
-            source_type=PEER,
-            source_url=self._external_url,
+            source_type="loki",
+            source_port=str(self._port),
         )
-
-        self.loki_provider = LokiPushApiProvider(
-            self,
-            address=external_url.hostname or self.hostname,
-            port=external_url.port or self._port,
-            scheme=external_url.scheme,
-            path=f"{external_url.path}/loki/api/v1/push",
-        )
+        self._loki_server = LokiServer()
+        self.loki_provider = LokiPushApiProvider(self)
 
         self.framework.observe(self.on.config_changed, self._on_config_changed)
         self.framework.observe(self.on.upgrade_charm, self._on_upgrade_charm)
@@ -88,47 +91,37 @@ class LokiOperatorCharm(CharmBase):
         )
         self.framework.observe(self.ingress_per_unit.on.ingress_changed, self._on_ingress_changed)
 
-        self._loki_server = LokiServer(base_path=external_url.path)
-        self._provide_loki()
-
     ##############################################
     #           CHARM HOOKS HANDLERS             #
     ##############################################
-    def _on_config_changed(self, event):
-        self._configure(event)
+    def _on_config_changed(self, _):
+        self._configure()
 
-    def _on_upgrade_charm(self, event):
-        self._configure(event)
+    def _on_upgrade_charm(self, _):
+        self._configure()
 
-    def _on_loki_pebble_ready(self, event):
-        self._configure(event)
+    def _on_loki_pebble_ready(self, _):
+        self._configure()
 
-    def _on_alertmanager_change(self, event):
-        self._configure(event)
-
-    def _loki_push_api_alert_rules_changed(self, event):
-        self._configure(event)
+    def _on_alertmanager_change(self, _):
+        self._configure()
 
     def _on_ingress_changed(self, event):
-        self._configure(event)
+        self._configure()
         self.loki_provider.update_endpoint(url=self._external_url)
 
-    def _configure(self, event):
+    def _configure(self):
         """Configure Loki charm."""
         restart = False
 
-        if isinstance(event, LokiPushApiAlertRulesChanged) and event.error:
-            self.unit.status = BlockedStatus(event.message)
-            return False
-
         if not self._container.can_connect():
             self.unit.status = WaitingStatus("Waiting for Pebble ready")
-            return False
+            return
 
-        current_layer = self._container.get_plan().services
+        current_layer = self._container.get_plan()
         new_layer = self._build_pebble_layer
 
-        if current_layer != new_layer:
+        if current_layer.services != new_layer.services:
             restart = True
 
         config = self._loki_config()
@@ -136,20 +129,30 @@ class LokiOperatorCharm(CharmBase):
         try:
             if yaml.safe_load(self._stored.config) != config:
                 config_as_yaml = yaml.safe_dump(config)
-                self._container.push(LOKI_CONFIG, config_as_yaml)
+                self._container.push(LOKI_CONFIG, config_as_yaml, make_dirs=True)
                 logger.info("Pushed new configuration")
                 self._stored.config = config_as_yaml
                 restart = True
         except (ProtocolError, PathError) as e:
             self.unit.status = BlockedStatus(str(e))
-            return False
+            return
+        except Exception as e:
+            self.unit.status = BlockedStatus(str(e))
+            return
 
         if restart:
             self._container.add_layer(self._name, new_layer, combine=True)
             self._container.restart(self._name)
             logger.info("Loki (re)started")
 
-        self.grafana_source_provider.update_source(self._external_url)
+        # Don't clear alert error states on reconfigure
+        # but let errors connecting clear after a restart
+        if (
+            isinstance(self.unit.status, BlockedStatus)
+            and "Errors in alert rule" in self.unit.status.message
+        ):
+            return
+
         self.unit.status = ActiveStatus()
 
     @property
@@ -163,39 +166,44 @@ class LokiOperatorCharm(CharmBase):
         return f"/usr/bin/loki -config.file={LOKI_CONFIG}"
 
     @property
-    def _build_pebble_layer(self):
+    def _build_pebble_layer(self) -> Layer:
         """Construct the pebble layer.
 
         Returns:
             a Pebble layer specification for the Loki workload container.
         """
-        pebble_layer = {
-            "summary": "Loki layer",
-            "description": "pebble config layer for Loki",
-            "services": {
-                "loki": {
-                    "override": "replace",
-                    "summary": "loki",
-                    "command": self._loki_command,
-                    "startup": "enabled",
+        pebble_layer = Layer(
+            {
+                "summary": "Loki layer",
+                "description": "pebble config layer for Loki",
+                "services": {
+                    "loki": {
+                        "override": "replace",
+                        "summary": "loki",
+                        "command": self._loki_command,
+                        "startup": "enabled",
+                    },
                 },
-            },
-        }
+            }
+        )
 
         return pebble_layer
 
     ##############################################
     #             UTILITY METHODS                #
     ##############################################
-    def _provide_loki(self):
+    def _loki_ready(self) -> bool:
         """Gets LokiPushApiProvider instance into `self.loki_provider`."""
         try:
             version = self._loki_server.version
             logger.debug("Loki Provider is available. Loki version: %s", version)
+            return True
         except LokiServerNotReadyError as e:
             self.unit.status = WaitingStatus(str(e))
+            return False
         except LokiServerError as e:
             self.unit.status = BlockedStatus(str(e))
+            return False
 
     def _alerting_config(self) -> str:
         """Construct Loki altering configuration.
@@ -222,8 +230,6 @@ class LokiOperatorCharm(CharmBase):
         Returns:
             Dictionary representation of the Loki YAML config
         """
-        external_url = urlparse(self._external_url)
-        no_path = "''"
         config = textwrap.dedent(
             f"""
             target: all
@@ -232,7 +238,7 @@ class LokiOperatorCharm(CharmBase):
             server:
               http_listen_port: {self._port}
               http_listen_address: 0.0.0.0
-              http_path_prefix: {external_url.path if external_url.path else no_path}
+
             common:
               path_prefix: {LOKI_DIR}
               storage:
@@ -241,7 +247,7 @@ class LokiOperatorCharm(CharmBase):
                   rules_directory: {RULES_DIR}
               replication_factor: 1
               ring:
-                instance_addr: {self._pod_ip}
+                instance_addr: {self.loki_provider.unit_ip if self.loki_provider else ""}
                 kvstore:
                   store: inmemory
 
@@ -261,22 +267,106 @@ class LokiOperatorCharm(CharmBase):
         )
         return yaml.safe_load(config)
 
-    @property
-    def _pod_ip(self) -> str:
-        """Returns the pod ip of this unit."""
-        if bind_address := self.model.get_binding(LOKI_PEERS).network.bind_address:
-            return str(bind_address)
-        return ""
+    def _loki_push_api_alert_rules_changed(self, _: LokiPushApiAlertRulesChanged) -> None:
+        """Perform all operations needed to keep alert rules in the right status."""
+        if self._ensure_alert_rules_path():
+            self._regenerate_alert_rules()
+
+            # Don't try to configure if checking the rules left us in BlockedStatus
+            if isinstance(self.unit.status, ActiveStatus):
+                self._configure()
+
+    def _ensure_alert_rules_path(self) -> bool:
+        """Ensure that the workload container has the appropriate directory structure."""
+        # create tenant dir so that the /loki/api/v1/rules endpoint returns "no rule groups found"
+        # instead of "unable to read rule dir /loki/rules/fake: no such file or directory"
+        if self._container.can_connect():
+            try:
+                self._container.make_dir(self._rules_dir, make_parents=True)
+                return True
+            except (FileNotFoundError, ProtocolError, PathError):
+                logger.debug("Could not create loki directory.")
+                return False
+            except Exception as e:
+                logger.debug("Could create loki directory: %s", e)
+                return False
+        return False
+
+    def _regenerate_alert_rules(self):
+        """Recreate all alert rules."""
+        self._remove_alert_rules_files()
+        # If there aren't any alerts, we can just clean it and move on
+        if self.loki_provider.alerts:
+            self._generate_alert_rules_files()
+            self._check_alert_rules()
+
+    def _generate_alert_rules_files(self) -> None:
+        """Generate and upload alert rules files.
+
+        Args:
+            container: Container into which alert rules files are going to be uploaded
+        """
+        file_mappings = {}
+
+        for identifier, alert_rules in self.loki_provider.alerts.items():
+            rules = yaml.dump({"groups": alert_rules["groups"]})
+            file_mappings["{}_alert.rules".format(identifier)] = rules
+
+        if self._container.can_connect():
+            for filename, content in file_mappings.items():
+                path = os.path.join(self._rules_dir, filename)
+                self._container.push(path, content, make_dirs=True)
+        logger.debug("Saved alert rules to disk")
+
+    def _remove_alert_rules_files(self) -> None:
+        """Remove alert rules files from workload container."""
+        if not self._container.can_connect():
+            logger.debug("Cannot connect to container to remove alert rule files!")
+            return
+
+        files = self._container.list_files(self._rules_dir)
+        for f in files:
+            self._container.remove_path(f.path)
+
+    def _check_alert_rules(self):
+        """Check alert rules using Loki API."""
+        url = "http://127.0.0.1:{}/loki/api/v1/rules".format(self.loki_provider.port)
+        req = request.Request(url)
+        try:
+            request.urlopen(req, timeout=2.0)
+        except HTTPError as e:
+            msg = e.read().decode("utf-8")
+
+            if e.code == 404 and "no rule groups found" in msg:
+                log_msg = "Checking alert rules: No rule groups found"
+                logger.debug(log_msg)
+                self.unit.status = BlockedStatus(log_msg)
+                return
+
+            message = "{} - {}".format(e.code, e.msg)  # type: ignore
+            logger.error("Checking alert rules: %s", message)
+            self.unit.status = BlockedStatus("Errors in alert rule groups. Check juju debug-log")
+            return
+        except URLError as e:
+            logger.error("Checking alert rules: %s", e.reason)
+            self.unit.status = BlockedStatus("Error connecting to Loki. Check juju debug-log")
+            return
+        except Exception as e:
+            logger.error("Checking alert rules: %s", e)
+            self.unit.status = BlockedStatus("Error connecting to Loki. Check juju debug-log")
+            return
+        else:
+            log_msg = "Checking alert rules: Ok"
+            logger.debug(log_msg)
+            if isinstance(self.unit.status, BlockedStatus):
+                self.unit.status = ActiveStatus()
+                logger.info("Clearing blocked status with successful alert rule check")
+            return
 
     @property
     def hostname(self) -> str:
         """Unit's hostname."""
-        return "{}-{}.{}-endpoints.{}.svc.cluster.local".format(
-            self.app.name,
-            self.unit.name.split("/")[-1],
-            self.app.name,
-            self.model.name,
-        )
+        return socket.getfqdn()
 
     @property
     def _external_url(self) -> str:
