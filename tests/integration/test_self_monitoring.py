8acb7501be909321684ad9e72bc1482524030352
Dylan Stephano-Shachter <dylan.stephano-shachter@canonical.com>
2022-07-20 14:14:48 +0000

    self monitoring integration tests

---
+++ tests/integration/test_self_monitoring.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# Copyright 2021 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import asyncio
+import logging
+
+import pytest
+import requests
+from helpers import get_unit_address, loki_endpoint_request, oci_image
+from requests.auth import HTTPBasicAuth
+
+logger = logging.getLogger(__name__)
+
+LOKI = "loki"
+GRAFANA = "grafana"
+PROMETHEUS = "prometheus"
+
+
+class AddressNotFoundError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+@pytest.mark.abort_on_fail
+async def test_deploy_and_relate_charms(ops_test, loki_charm):
+    """Test that Prometheus can be related with the Grafana Agent over remote_write."""
+    await asyncio.gather(
+        ops_test.model.deploy(
+            loki_charm,
+            resources={"loki-image": oci_image("./metadata.yaml", "loki-image")},
+            application_name=LOKI,
+        ),
+        ops_test.model.deploy(
+            "grafana-k8s",
+            application_name=GRAFANA,
+            channel="edge",
+        ),
+        ops_test.model.deploy("prometheus-k8s", application_name=PROMETHEUS, channel="edge"),
+    )
+
+    await ops_test.model.add_relation(LOKI, PROMETHEUS)
+    await ops_test.model.add_relation(PROMETHEUS, f"{GRAFANA}:grafana-source")
+    await ops_test.model.add_relation(GRAFANA, f"{LOKI}:grafana-dashboard")
+    apps = [LOKI, GRAFANA, PROMETHEUS]
+    await ops_test.model.wait_for_idle(apps=apps, status="active")
+
+
+async def test_metrics_are_available(ops_test):
+    metrics = await loki_endpoint_request(ops_test, LOKI, "metrics", 0)
+    assert len(metrics) > 0
+
+
+async def test_query_metrics_from_prometheus(ops_test):
+    address = await get_unit_address(ops_test, PROMETHEUS, 0)
+    url = f"http://{address}:9090/api/v1/query"
+    params = {"query": f"up{{juju_application='{LOKI}'}}"}
+    try:
+        response = requests.get(url, params=params)
+        assert response.json()["status"] == "success"
+    except requests.exceptions.RequestException:
+        assert False
+
+
+async def test_dashboard_exists(ops_test):
+    address = await get_unit_address(ops_test, GRAFANA, 0)
+    pw_action = (
+        await ops_test.model.applications[GRAFANA].units[0].run_action("get-admin-password")
+    )
+    pw_action = await pw_action.wait()
+    pw = pw_action.results["admin-password"]
+    url = f"http://{address}:3000/api/dashboards/tags"
+    auth = HTTPBasicAuth("admin", pw)
+    params = {"tag": LOKI}
+    try:
+        response = requests.get(url, auth=auth, params=params)
+        assert response.json()
+    except requests.exceptions.RequestException:
+        assert False
