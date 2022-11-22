274df0421bbcaab7d25e3cceea1075c80c29f2db
sed-i <82407168+sed-i@users.noreply.github.com>
2022-11-22 12:29:06 -0500

    no_implicit_optional

--- lib/charms/loki_k8s/v0/loki_push_api.py
+++ lib/charms/loki_k8s/v0/loki_push_api.py
@@ -1229,7 +1229,7 @@ class LokiPushApiProvider(Object):
 
         return {"promtail_binary_zip_url": json.dumps(promtail_binaries)}
 
-    def update_endpoint(self, url: str = "", relation: Relation = None) -> None:
+    def update_endpoint(self, url: str = "", relation: Optional[Relation] = None) -> None:
         """Triggers programmatically the update of endpoint in unit relation data.
 
         This method should be used when the charm relying on this library needs
@@ -1676,7 +1676,7 @@ class LogProxyConsumer(ConsumerBase):
     def __init__(
         self,
         charm,
-        log_files: list = None,
+        log_files: Optional[list] = None,
         relation_name: str = DEFAULT_LOG_PROXY_RELATION_NAME,
         enable_syslog: bool = False,
         syslog_port: int = 1514,
