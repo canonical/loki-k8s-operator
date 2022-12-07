59847e4451b53a4e51933f0364f2463c77587b32
Luca Bello <luca.bello@canonical.com>
2022-12-07 17:32:01 +0100

    update libraries and xfail scaledown test

--- tests/integration/test_alert_rules_fire.py
+++ tests/integration/test_alert_rules_fire.py
@@ -91,7 +91,7 @@ async def test_loki_scales_up(ops_test):
         )
 
 
-@pytest.mark.abort_on_fail
+@pytest.mark.xfail
 async def test_scale_down_to_zero_units(ops_test):
     loki_app_name = "loki"
     await ops_test.model.applications[loki_app_name].scale(scale=0)
