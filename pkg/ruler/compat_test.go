b5a767aa4148de9e0257f09a67c6ebbc5362cb4e
Michel Hollands <42814411+MichelHollands@users.noreply.github.com>
2023-07-21 15:18:08 +0100

    Backport go 1.20.6 to 2.8 (#9944)
    **What this PR does / why we need it**:
    
    Update the 2.8 branch so it uses Go version 1.20.6. A 2.8.x release will
    be built from this later on.
    
    **Checklist**
    - [X] Reviewed the
    [`CONTRIBUTING.md`](https://github.com/grafana/loki/blob/main/CONTRIBUTING.md)
    guide (**required**)
    - [ ] Documentation added
    - [ ] Tests updated
    - [ ] `CHANGELOG.md` updated
    - [ ] If the change is worth mentioning in the release notes, add
    `add-to-release-notes` label
    - [ ] Changes that require user attention or interaction to upgrade are
    documented in `docs/sources/upgrading/_index.md`
    - [ ] For Helm chart changes bump the Helm chart version in
    `production/helm/loki/Chart.yaml` and update
    `production/helm/loki/CHANGELOG.md` and
    `production/helm/loki/README.md`. [Example
    PR](https://github.com/grafana/loki/commit/d10549e3ece02120974929894ee333d07755d213)
    
    ---------
    
    Signed-off-by: Michel Hollands <michel.hollands@grafana.com>
    Signed-off-by: Michel Hollands <michel.hollands@gmail.com>

---
+++ pkg/ruler/compat_test.go
@@ -0,0 +1,69 @@
+package ruler
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/prometheus/prometheus/config"
+	"github.com/stretchr/testify/require"
+
+	"github.com/grafana/loki/pkg/iter"
+	"github.com/grafana/loki/pkg/logql"
+	ruler "github.com/grafana/loki/pkg/ruler/base"
+	"github.com/grafana/loki/pkg/util/log"
+	"github.com/grafana/loki/pkg/validation"
+)
+
+// TestInvalidRemoteWriteConfig tests that a validation error is raised when config is invalid
+func TestInvalidRemoteWriteConfig(t *testing.T) {
+	// if remote-write is not enabled, validation fails
+	cfg := Config{
+		Config: ruler.Config{},
+		RemoteWrite: RemoteWriteConfig{
+			Enabled: false,
+		},
+	}
+	require.Nil(t, cfg.RemoteWrite.Validate())
+
+	// if no remote-write URL is configured, validation fails
+	cfg = Config{
+		Config: ruler.Config{},
+		RemoteWrite: RemoteWriteConfig{
+			Enabled: true,
+			Client: &config.RemoteWriteConfig{
+				URL: nil,
+			},
+		},
+	}
+	require.Error(t, cfg.RemoteWrite.Validate())
+}
+
+// TestNonMetricQuery tests that only metric queries can be executed in the query function,
+// as both alert and recording rules rely on metric queries being run
+func TestNonMetricQuery(t *testing.T) {
+	overrides, err := validation.NewOverrides(validation.Limits{}, nil)
+	require.Nil(t, err)
+
+	engine := logql.NewEngine(logql.EngineOpts{}, &FakeQuerier{}, overrides, log.Logger)
+	queryFunc := engineQueryFunc(engine, overrides, fakeChecker{}, "fake")
+
+	_, err = queryFunc(context.TODO(), `{job="nginx"}`, time.Now())
+	require.Error(t, err, "rule result is not a vector or scalar")
+}
+
+type FakeQuerier struct{}
+
+func (q *FakeQuerier) SelectLogs(context.Context, logql.SelectLogParams) (iter.EntryIterator, error) {
+	return iter.NoopIterator, nil
+}
+
+func (q *FakeQuerier) SelectSamples(context.Context, logql.SelectSampleParams) (iter.SampleIterator, error) {
+	return iter.NoopIterator, nil
+}
+
+type fakeChecker struct{}
+
+func (f fakeChecker) isReady(tenant string) bool {
+	return true
+}
