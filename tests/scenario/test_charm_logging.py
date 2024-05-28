61babd6d470c2bbc2673b5da16ff8e803f1d5a21
Pietro Pasotti <starfire.daemon@gmail.com>
2024-05-28 14:22:18 +0200

    enabled scenarios

--- tests/scenario/test_charm_logging.py
+++ tests/scenario/test_charm_logging.py
@@ -7,12 +7,12 @@ import scenario
 
 
 @pytest.fixture
-def loki_handler_mock():
-    with patch("logging_loki.LokiHandler.handle") as h:
+def loki_emitter():
+    with patch("charms.loki_k8s.v0.charm_logging.LokiHandler.emit") as h:
         yield h
 
 
-def test_no_endpoints_on_loki_not_ready(context, loki_handler_mock):
+def test_no_endpoints_on_loki_not_ready(context, loki_emitter):
     state = scenario.State(
         containers=[
             scenario.Container(
@@ -30,10 +30,10 @@ def test_no_endpoints_on_loki_not_ready(context, loki_handler_mock):
         assert charm.logging_endpoints == []
         logging.getLogger("foo").debug("bar")
 
-    loki_handler_mock.assert_not_called()
+    loki_emitter.assert_not_called()
 
 
-def test_endpoints_on_loki_ready(context, loki_handler_mock):
+def test_endpoints_on_loki_ready(context, loki_emitter):
     state = scenario.State(
         containers=[
             scenario.Container(
@@ -48,12 +48,40 @@ def test_endpoints_on_loki_ready(context, loki_handler_mock):
 
     with context.manager("update-status", state) as mgr:
         charm = mgr.charm
-        assert charm.logging_endpoints == ["http://fqdn:80/loki/api/v1/push"]
+        assert charm.logging_endpoints == ["http://localhost:3100/loki/api/v1/push"]
         logging.getLogger("foo").debug("bar")
 
-    loki_handler_mock.assert_called()
+    loki_emitter.assert_called()
 
-    for call in loki_handler_mock.call_args_list:
+    for call in loki_emitter.call_args_list:
+        record = call.args[0]
+        if record.filename == __name__ + ".py":  # log emitted by this module
+            assert record.msg == "bar"
+            assert record.name == "foo"
+
+
+@patch("charm.LokiOperatorCharm.server_ca_cert_path", new_callable=lambda *_: True)
+def test_endpoints_on_loki_ready_tls(_, context, loki_emitter):
+    state = scenario.State(
+        containers=[
+            scenario.Container(
+                "loki",
+                can_connect=True,
+                layers={"loki": ops.pebble.Layer({"services": {"loki": {}}})},
+                service_status={"loki": ops.pebble.ServiceStatus.ACTIVE},
+                exec_mock={("update-ca-certificates", "--fresh"): scenario.ExecOutput()},
+            )
+        ]
+    )
+
+    with context.manager("update-status", state) as mgr:
+        charm = mgr.charm
+        assert charm.logging_endpoints == ["https://localhost:3100/loki/api/v1/push"]
+        logging.getLogger("foo").debug("bar")
+
+    loki_emitter.assert_called()
+
+    for call in loki_emitter.call_args_list:
         record = call.args[0]
         if record.filename == __name__ + ".py":  # log emitted by this module
             assert record.msg == "bar"
