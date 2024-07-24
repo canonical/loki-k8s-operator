471bc44dbbe1502e4fa12bbefc4190fe16551ec3
Jose C. Massón <939888+Abuelodelanada@users.noreply.github.com>
2024-07-24 10:37:23 -0300

    Update lib/charms/loki_k8s/v1/loki_push_api.py
    Co-authored-by: Leon <82407168+sed-i@users.noreply.github.com>

--- lib/charms/loki_k8s/v1/loki_push_api.py
+++ lib/charms/loki_k8s/v1/loki_push_api.py
@@ -2562,10 +2562,9 @@ class LogForwarder(ConsumerBase):
             return
 
         for container in self._charm.unit.containers.values():
-            if not container.can_connect():
-                continue
-
-            self._update_endpoints(container, loki_endpoints)
+            if container.can_connect():
+                self._update_endpoints(container, loki_endpoints)
+            # else: `_update_endpoints` will be called on pebble-ready anyway.
 
     def _retrieve_endpoints_from_relation(self) -> dict:
         loki_endpoints = {}
