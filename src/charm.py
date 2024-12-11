51fe877e8317e5175d499ce980a5bea7ce699716
michael <michael.dmitry98@gmail.com>
2024-12-11 13:57:12 +0200

    fetch lib

--- src/charm.py
+++ src/charm.py
@@ -898,6 +898,9 @@ class LokiOperatorCharm(CharmBase):
 
     def _update_datasource_exchange(self) -> None:
         """Update the grafana-datasource-exchange relations."""
+        if not self.unit.is_leader():
+            return
+
         grafana_uids_to_units_to_uids = self.grafana_source_provider.get_source_uids()
         raw_datasources: List[DatasourceDict] = []
 
