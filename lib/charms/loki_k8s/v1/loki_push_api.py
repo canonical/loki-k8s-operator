a3b67c99bf410c895b5cafb5818bc958f5357add
Luca Bello <luca.bello@canonical.com>
2025-02-06 18:05:10 +0100

    bump libpatch

--- lib/charms/loki_k8s/v1/loki_push_api.py
+++ lib/charms/loki_k8s/v1/loki_push_api.py
@@ -546,7 +546,7 @@ LIBAPI = 1
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 13
+LIBPATCH = 14
 
 PYDEPS = ["cosl"]
 
