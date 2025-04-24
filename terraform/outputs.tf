569ed63185760cbc06e29b725129d115a04d9606
Simon Aronsson <simme@arcticbit.se>
2025-04-24 09:13:01 +0200

    fix(linting): apply terraform lint suggestions

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,20 +6,20 @@ output "endpoints" {
   value = {
     # Requires
 
-    alertmanager          = "alertmanager"
-    ingress               = "ingress"
-    catalogue             = "catalogue"
-    certificates          = "certificates"
-    charm_tracing         = "charm-tracing"
-    workload_tracing      = "workload-tracing"
+    alertmanager     = "alertmanager"
+    ingress          = "ingress"
+    catalogue        = "catalogue"
+    certificates     = "certificates"
+    charm_tracing    = "charm-tracing"
+    workload_tracing = "workload-tracing"
 
     # Provides
 
-    metrics_endpoint = "metrics-endpoint"
-    grafana_source        = "grafana-source"
-    grafana_dashboard     = "grafana-dashboard"
-    receive_remote_write  = "receive-remote-write"
-    send_datasource       = "send-datasource"
-    logging               = "logging"
+    metrics_endpoint     = "metrics-endpoint"
+    grafana_source       = "grafana-source"
+    grafana_dashboard    = "grafana-dashboard"
+    receive_remote_write = "receive-remote-write"
+    send_datasource      = "send-datasource"
+    logging              = "logging"
   }
 }
