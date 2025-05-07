ea1438111b61d70b30032df3a9e81739cfbb1973
Michael Thamm <mike.thamm@canonical.com>
2025-05-07 10:25:24 -0400

    chore: drive-by docs fix

--- lib/charms/loki_k8s/v0/charm_logging.py
+++ lib/charms/loki_k8s/v0/charm_logging.py
@@ -12,8 +12,7 @@ consumers/forwarders from the ``loki_push_api`` library, you need to:
     charmcraft fetch-lib charms.loki_k8s.v1.loki_push_api
 
 and add the logging consumer that matches your use case.
-See https://charmhub.io/loki-k8s/libraries/loki_push_apihttps://charmhub.io/loki-k8s/libraries/loki_push_api
-for more information.
+See [loki_push_api](https://charmhub.io/loki-k8s/libraries/loki_push_api) for more information.
 
 Once your charm is related to, for example, COS' Loki charm (or a Grafana Agent),
 you will be able to inspect in real time from the Grafana dashboard the logs emitted by your charm.
@@ -97,7 +96,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 2
+LIBPATCH = 3
 
 PYDEPS = ["cosl"]
 
