e2dd673ef6c3eecc46b023e62709e812247f7a3c
Luca Bello <luca.bello@canonical.com>
2025-05-12 12:10:45 +0200

    update test for loki

--- tests/load/sprint.js
+++ tests/load/sprint.js
@@ -1,15 +1,17 @@
 import http from 'k6/http';
+import loki from 'k6/x/loki';
 import { check, sleep } from "k6";
 
 export const options = {
-    thresholds: {
+  vus: 10,
+  thresholds: {
     // Assert that 99% of requests finish within 3000ms.
     http_req_duration: ["p(99) < 3000"],
   },
   scenarios: {
-    pizza: {
+    send: {
       executor: 'ramping-vus',
-      exec: 'pizza',
+      exec: 'send',
       startVUs: 0,
       stages: [
         { duration: '20s', target: 10 },
@@ -17,9 +19,9 @@ export const options = {
       ],
       gracefulRampDown: '0s',
     },
-    news: {
+    query: {
       executor: 'ramping-vus',
-      exec: 'news',
+      exec: 'query',
       startVUs: 10,
       stages: [
         { duration: '20s', target: 10 },
@@ -31,18 +33,35 @@ export const options = {
 
 };
 
+
 //use the exec property to run different scenarios for different functions
 
-export function pizza() {
-  let res = http.get("https://quickpizza.grafana.com", {tags: {test: "pizza"}});
+export function send() {
+  const conf = loki.Config(`http://fake@${__ENV.LOKI_IP}:3100`)
+  const client = loki.Client(conf);
+
+  const streams = 4; // log streams per client
+  const minSize = 1024; // log line min: 1kb
+  const maxSize = 2048; // log line max: 2kb
+  const thinkTime = 1 // waiting time
+
+  const res = client.pushParametrized(streams, minSize, maxSize);
+
   // Validate response status
   check(res, { "status was 200": (r) => r.status == 200 });
   sleep(1); // Think time
 }
 
-export function news() {
-  let res = http.get("https://test.k6.io/news.php", {tags: {test: "news"}});
-  // Validate response status
-  check(res, { "status was 200": (r) => r.status == 200 });
-  sleep(1); // Think time
+export function query() {
+  const conf = loki.Config(`http://fake@${__ENV.LOKI_IP}:3100`)
+  const client = loki.Client(conf);
+
+  const thinkTime = 6
+  const query = '{instance=~"vu.+"}'
+  const queryRange = "30m"
+  const queryLimit = 600
+  const res = client.rangeQuery(query, queryRange, queryLimit)
+
+  check(res, { 'successful range query': (res) => res.status == 200 });
+  sleep(thinkTime)
 }
