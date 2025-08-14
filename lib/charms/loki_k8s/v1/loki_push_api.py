7bbde096608b50c2b8e7619324b1dd66f2753cf0
Luca Bello <luca.bello@canonical.com>
2025-08-14 10:46:17 +0200

    feat: add extra_alert_labels functionality to log alerts

--- lib/charms/loki_k8s/v1/loki_push_api.py
+++ lib/charms/loki_k8s/v1/loki_push_api.py
@@ -543,7 +543,7 @@ LIBAPI = 1
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 18
+LIBPATCH = 19
 
 PYDEPS = ["cosl"]
 
