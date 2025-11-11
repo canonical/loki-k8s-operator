61aaa5b4635dc3ec6e84fdfddb64dc93dd3b2d30
Erik Lönroth <erik.lonroth@gmail.com>
2025-11-11 10:57:33 +0100

    Update lib patch version to 22

--- lib/charms/loki_k8s/v1/loki_push_api.py
+++ lib/charms/loki_k8s/v1/loki_push_api.py
@@ -545,7 +545,7 @@ LIBAPI = 1
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 21
+LIBPATCH = 22
 
 PYDEPS = ["cosl"]
 
