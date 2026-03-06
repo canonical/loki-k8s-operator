5afe151a30183759203a06ca8a7d0d6cd5e78348
Michael Thamm <mike.thamm@canonical.com>
2026-03-06 15:11:40 -0500

    fix: Split TF endpoints output to requires/provides

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,24 +2,24 @@ output "app_name" {
   value = juju_application.loki.name
 }
 
-output "endpoints" {
+output "provides" {
   value = {
-    # Requires
-
-    alertmanager     = "alertmanager"
-    ingress          = "ingress"
-    catalogue        = "catalogue"
-    certificates     = "certificates"
-    charm_tracing    = "charm-tracing"
-    workload_tracing = "workload-tracing"
-
-    # Provides
-
     metrics_endpoint     = "metrics-endpoint"
     grafana_source       = "grafana-source"
     grafana_dashboard    = "grafana-dashboard"
     receive_remote_write = "receive-remote-write"
     send_datasource      = "send-datasource"
     logging              = "logging"
+  }
+}
+
+output "requires" {
+  value = {
+    alertmanager     = "alertmanager"
+    ingress          = "ingress"
+    catalogue        = "catalogue"
+    certificates     = "certificates"
+    charm_tracing    = "charm-tracing"
+    workload_tracing = "workload-tracing"
   }
 }
