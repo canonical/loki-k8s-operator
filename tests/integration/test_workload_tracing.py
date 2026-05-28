9186c58af418e46cec943603c798de9a3ecef339
Sina <sina.pahlavan@canonical.com>
2026-05-28 09:30:32 -0400

    feat: use patched Loki 3.7.1 rock

--- tests/integration/test_workload_tracing.py
+++ tests/integration/test_workload_tracing.py
@@ -30,9 +30,7 @@ loki_resources = {
 async def test_setup_env(ops_test):
     await ops_test.model.set_config({"logging-config": "<root>=WARNING; unit=DEBUG"})
 
-# Xfailing the test until the expected resolution date of the issue: https://github.com/canonical/loki-operators/issues/59
-# Using strict=True to ensure that the test will start failing if it starts passing before the expected date, which would indicate that the underlying issue has been resolved.
-@pytest.mark.xfail(date.today() < date(2026, 5, 19), reason="expected to fail until 2026-05-19", strict=True)
+
 async def test_workload_tracing_is_present(ops_test, loki_charm, cos_channel):
     # Deploy a Tempo cluster
     minio_user = "accesskey"
